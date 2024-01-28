43cf5033ddfacc7a42b47e37e6e126f1d16f5a0b
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-01-28 19:13:54 -0300

    v1.0
    all componentes complete, less certificates

--- src/components/SkillBar.vue
+++ src/components/SkillBar.vue
@@ -7,6 +7,10 @@ defineProps({
   percent: {
     type: String,
     required: true
+  },
+  level:{
+    type: String,
+    required: true
   }
 })
 </script>
@@ -15,7 +19,7 @@ defineProps({
   <div class="skillBar">
     <div class="skillBar__name">{{ name }}</div>
     <div class="skillBar__bar">
-      <div class="skillBar__percentage" :style="{width: `${percent}%`}">
+      <div class="skillBar__percentage" :style="{width: `${percent}%`, animation: `${level} 3s`}">
       </div>
     </div>
   </div>
@@ -34,16 +38,45 @@ defineProps({
   }
 
   .skillBar__bar {
-    height: 1rem;
     background-color: var(--dark-gray);
+    display: block;
+    height: 1rem;
+    outline: 3px solid var(--green-dark);
     border-radius: 0.25rem;
-    width: 100%;
+    overflow: hidden;
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    margin: 0.15rem 0;
 
     .skillBar__percentage {
       height: 1rem;
-      border-radius: 0.25rem;
-      background: linear-gradient(to left, var(--green), var(--green-opaque));
+      float: left;
+      background: linear-gradient(to left, var(--green), var(--green-dark));
       position: relative;
+    }
+
+    @keyframes advanced {
+      0%{
+        width: 0%;
+      }
+      100%{
+        width: 90%;
+      }
+    }
+    @keyframes intermediate {
+      0%{
+        width: 0%;
+      }
+      100%{
+        width: 60%;
+      }
+    }
+    @keyframes beginner {
+      0%{
+        width: 0%;
+      }
+      100%{
+        width: 25%;
+      }
     }
   }
 }
